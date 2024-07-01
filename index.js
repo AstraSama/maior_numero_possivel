2a69c73d0e2485f16c5790ae780a9ffe018af0c5
Felipe <fe_lipe-mar_tins@hotmail.com>
2024-07-01 10:33:01 -0300

    Mudança de nome

---
+++ index.js
@@ -0,0 +1,28 @@
+let numero = "";
+let aux = '';
+
+const prompt = require("prompt-sync")();
+
+const MAIORAL = (data) => {
+    data = data.split("");
+
+    for(let i = 0; i < data.length; i++) {
+        for(let j = 0; j < data.length; j++) {
+            if(data[i] > data[j]) {
+                aux = data[i];
+                data[i] = data[j];
+                data[j] = aux;
+
+            }
+        }
+    }
+    data = data.join("")
+
+    return data;
+}
+
+numero = prompt("Escreva um número: ");
+
+numero = ANAGRAMA(numero);
+console.log(numero);
+
